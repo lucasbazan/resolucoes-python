15d890b62f205f32ce89fe72e367e405b2762169
Lucas Bazan <lucas.bazan2000@gmail.com>
2021-04-18 18:08:00 -0300

    ex01.py
    Criar uma bola e dar cor à ela

---
+++ classes/ex01.py
@@ -0,0 +1,33 @@
+'''
+  Classe Bola: Crie uma classe que modele uma bola:
+  Atributos: Cor, circunferência, material
+  Métodos: trocaCor e mostraCor
+'''
+
+class Bola:
+  def __init__(self, cor='n/a', circ=0, mat='n/a'):
+    self.cor = cor # Cor
+    self.circ = circ # Circunferência
+    self.mat = mat # Material
+  
+  def trocaCor(self):
+    self.cor = input('Para qual cor gostaria de trocar? ')
+
+  def mostraCor(self):
+    print('A cor atual é {}'.format(self.cor))
+
+def main():
+  bola = Bola('n/a', 0, 'n/a')
+
+  while True:
+    bola.mostraCor()
+
+    escolha = input('Você gostaria de mudar a cor? [s/n]: ')
+    escolha = escolha[0].lower()
+
+    if escolha == 's':
+      bola.trocaCor()
+    else:
+      pass
+
+main()
