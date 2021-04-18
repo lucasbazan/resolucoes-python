c3a8d996f0d29b9c0f63dec5243da093dab8fddb
Lucas Bazan <lucas.bazan2000@gmail.com>
2021-04-18 18:16:59 -0300

    Exercício 02 - Classe Quadrado
    Crie uma classe que modele um quadrado

--- classes/ex02.py
+++ classes/ex02.py
@@ -1,6 +1,8 @@
-# Classe Quadrado: Crie uma classe que modele um quadrado:
-# Atributos: Tamanho do lado
-# Métodos: Mudar valor do Lado, Retornar valor do Lado e calcular Área;
+'''
+  Classe Quadrado: Crie uma classe que modele um quadrado:
+  Atributos: Tamanho do lado
+  Métodos: Mudar valor do Lado, Retornar valor do Lado e calcular Área;
+'''
 
 class Quadrado:
   def __init__(self, lado=0):
