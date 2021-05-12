281ce6996026c4658c6f6f3cb4bc2258e5ac6c2b
Lucas Bazan <lucas.bazan2000@gmail.com>
2021-05-12 17:14:15 -0300

    Create 3.py

---
+++ EstruturaSequencial/3.py
@@ -0,0 +1,5 @@
+#Faça um Programa que peça dois números e imprima a soma. 
+n1 = int(input("Insira o primeiro numero: "))
+n2 = int(input("Insira o segundo numero: "))
+
+print(f"A soma dos dois numero equivale a {n1 + n2}")
