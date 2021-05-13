e6c51aae724ddf3ebfd7a061828a48d199046211
Lucas Bazan <lucas.bazan2000@gmail.com>
2021-05-13 14:29:18 -0300

    Update 18.py

--- EstruturaSequencial/18.py
+++ EstruturaSequencial/18.py
@@ -4,8 +4,8 @@ a velocidade de um link de Internet (em Mbps), calcule e informe o
 tempo aproximado de download do arquivo usando este link (em minutos). 
 '''
 def calcular_download(mb, mbps):
-  tempo = int(mb / (mbps / 8) / 60)
-  print(f"O tempo aproximado de download equivale a {tempo} minuto(s)")
+  tempo = mb / (mbps / 8) / 60
+  print(f"O tempo aproximado de download equivale a {int(tempo)} minuto(s)")
 
 tamanho = float(input("Insira o tamanho do arquivo em MB: "))
 velocidade = float(input("Insira a velocidade da internet em Mbps: "))
