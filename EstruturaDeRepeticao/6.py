0f826a76b5f2791c05f573e55f727130e7609c03
Lucas Bazan <lucas.bazan2000@gmail.com>
2021-05-14 09:55:23 -0300

    Create 6.py

---
+++ EstruturaDeRepeticao/6.py
@@ -0,0 +1,9 @@
+'''
+Faça um programa que imprima na tela os números de 1 a 20, um abaixo do outro. 
+Depois modifique o programa para que ele mostre os números um ao lado do outro. 
+'''
+for i in range(1, 21): #Imprime um em baixo do outro
+  print(i)
+
+for i in range(1, 21): #Imprime um ao lado do outro
+  print(i, end = " ")
