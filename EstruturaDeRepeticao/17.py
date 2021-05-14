8c690c72fc6b6fba38cd4e93db3338a56e8ce71d
Lucas Bazan <lucas.bazan2000@gmail.com>
2021-05-14 15:51:43 -0300

    Create 17.py

---
+++ EstruturaDeRepeticao/17.py
@@ -0,0 +1,14 @@
+'''
+Faça um programa que calcule o fatorial de um 
+número inteiro fornecido pelo usuário. Ex.: 5!=5.4.3.2.1=120
+'''
+n = int(input("Insira o numero: "))
+_n = n
+i = n
+
+while i > 1:
+  f.append(i)
+  i -= 1
+  n = n * i
+
+print(f"{_n}!={n}")
